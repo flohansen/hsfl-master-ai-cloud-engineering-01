a9c02feee905b1c7ed0c03613bfe2d999a7694d1
Florian Hansen <flhansen.93@gmail.com>
2023-10-05 01:30:42 +0200

    feat(product-service): implement route to create products

--- src/product-service/products/default_controller.go
+++ src/product-service/products/default_controller.go
@@ -3,7 +3,20 @@ package products
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/flohansen/hsfl-master-ai-cloud-engineering/product-service/products/model"
 )
+
+type createProductRequest struct {
+	Name        string  `json:"name"`
+	Retailer    string  `json:"retailer"`
+	Price       float32 `json:"price"`
+	Description string  `json:"description"`
+}
+
+func (r createProductRequest) isValid() bool {
+	return r.Name != "" && r.Retailer != ""
+}
 
 type DefaultController struct {
 	productRepository Repository
@@ -26,7 +39,27 @@ func (ctrl *DefaultController) GetProducts(w http.ResponseWriter, r *http.Reques
 	json.NewEncoder(w).Encode(products)
 }
 
-func (ctrl *DefaultController) PostProducts(http.ResponseWriter, *http.Request) {
+func (ctrl *DefaultController) PostProducts(w http.ResponseWriter, r *http.Request) {
+	var request createProductRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !request.isValid() {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := ctrl.productRepository.Create([]*model.Product{{
+		Name:        request.Name,
+		Retailer:    request.Retailer,
+		Price:       request.Price,
+		Description: request.Description,
+	}}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func (ctrl *DefaultController) GetProduct(http.ResponseWriter, *http.Request) {
--- src/product-service/products/default_controller_test.go
+++ src/product-service/products/default_controller_test.go
@@ -3,8 +3,10 @@ package products
 import (
 	"encoding/json"
 	"errors"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	mocks "github.com/flohansen/hsfl-master-ai-cloud-engineering/product-service/_mocks"
@@ -66,10 +68,79 @@ func TestDefaultController(t *testing.T) {
 	})
 
 	t.Run("PostProducts", func(t *testing.T) {
+		t.Run("should return 400 BAD REQUEST if payload is not json", func(t *testing.T) {
+			tests := []io.Reader{
+				nil,
+				strings.NewReader(`{"invalid`),
+			}
+
+			for _, test := range tests {
+				// given
+				w := httptest.NewRecorder()
+				r := httptest.NewRequest("POST", "/api/v1/products", test)
+
+				// when
+				controller.PostProducts(w, r)
+
+				// then
+				assert.Equal(t, http.StatusBadRequest, w.Code)
+			}
+		})
+
+		t.Run("should return 400 BAD REQUEST if payload is incomplete", func(t *testing.T) {
+			tests := []io.Reader{
+				strings.NewReader(`{"price": 99.99}`),
+				strings.NewReader(`{"description": "amazing product"}`),
+				strings.NewReader(`{"retailer": "the company"}`),
+			}
+
+			for _, test := range tests {
+				// given
+				w := httptest.NewRecorder()
+				r := httptest.NewRequest("POST", "/api/v1/products", test)
+
+				// when
+				controller.PostProducts(w, r)
+
+				// then
+				assert.Equal(t, http.StatusBadRequest, w.Code)
+			}
+		})
+
+		t.Run("should return 500 INTERNAL SERVER ERROR if persisting failed", func(t *testing.T) {
+			// given
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("POST", "/api/v1/products",
+				strings.NewReader(`{"name":"test product","retailer":"the company"}`))
+
+			productRepository.
+				EXPECT().
+				Create([]*model.Product{{Name: "test product", Retailer: "the company"}}).
+				Return(errors.New("database error"))
+
+			// when
+			controller.PostProducts(w, r)
+
+			// then
+			assert.Equal(t, http.StatusInternalServerError, w.Code)
+		})
+
 		t.Run("should create new product", func(t *testing.T) {
 			// given
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("POST", "/api/v1/products",
+				strings.NewReader(`{"name":"test product","retailer":"the company"}`))
+
+			productRepository.
+				EXPECT().
+				Create([]*model.Product{{Name: "test product", Retailer: "the company"}}).
+				Return(nil)
+
 			// when
+			controller.PostProducts(w, r)
+
 			// then
+			assert.Equal(t, http.StatusOK, w.Code)
 		})
 	})
 
